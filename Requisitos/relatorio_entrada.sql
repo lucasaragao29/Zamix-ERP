6a74b33e70cc0e75abe6f01ae71e99a19adeea1f
lucasaragao29 <95982348+lucasaragao29@users.noreply.github.com>
2022-12-15 07:43:17 -0300

    Update relatorio_entrada.sql

--- Requisitos/relatorio_entrada.sql
+++ Requisitos/relatorio_entrada.sql
@@ -1,5 +1,5 @@
 select nome_prod_prod and nome_prod_comp and preco_compra and preco_venda and quantidade
- data_entrada =< yyyy/mm/dd and data_entrada >= yyyy/mm/dd 
+ where (data_entrada =< yyyy/mm/dd/)and (data_entrada >= yyyy/mm/dd) 
 from produtos p
 inner join produto_composto pc on pc.id=p.id;
 (select nome_prod_prod,nome_prod_comp,preco_compra,preco_venda,quantidade from produtos p
