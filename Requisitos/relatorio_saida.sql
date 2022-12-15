14fca558d3eeab0b11508b28641b754ce9df7e9d
lucasaragao29 <95982348+lucasaragao29@users.noreply.github.com>
2022-12-15 07:44:37 -0300

    Update relatorio_saida.sql

--- Requisitos/relatorio_saida.sql
+++ Requisitos/relatorio_saida.sql
@@ -1,5 +1,6 @@
 select nome_prod_prod and nome_prod_comp and preco_compra and  (preco_custo* quantidade)
-quantidade and data_retirada =< yyyy/mm/dd and data_retirada >= yyyy/mm/dd 
+quantidade; 
+where(data_retirada =< yyyy/mm/dd) and (data_retirada >= yyyy/mm/dd) 
 from produtos p
 inner join produto_composto pc on pc.id=p.id;
 (select nome_prod_prod,nome_prod_comp,preco_compra,
